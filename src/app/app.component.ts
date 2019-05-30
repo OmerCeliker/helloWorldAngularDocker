fab78b06866f957a7081b563a50979c58612b7e8
OmerCeliker <OmerCeliker@users.noreply.github.com>
2019-05-30 15:01:04 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My First Angular App  v2, thanks for attending!!!';
+  title = 'My First Angular App  v3';
 }
